bac95410697e0ecb43f69d066fc8cd72c22469b6
Anish Agarwal <anish.agarwal@solutionanalysts.com>
2020-06-11 21:19:37 +0530

    [fix] typo fixed

--- www/AppList.js
+++ www/AppList.js
@@ -1,12 +1,12 @@
 var AppList = {
 	applist : function (success, error) {
-		cordova.exec((success, error, 'AppList', 'applist', []));
+		cordova.exec(success, error, 'AppList', 'applist', []);
 	},
 	appicon: function (pkg, success, error) {
-		cordova.exec((success, error, 'AppList', 'appicon', [pkg]));
+		cordova.exec(success, error, 'AppList', 'appicon', [pkg]);
 	},
 	appstart: function (pkg, success, error) {
-		cordova.exec((success, error, 'AppList', 'appstart', [pkg]));
+		cordova.exec(success, error, 'AppList', 'appstart', [pkg]);
 	}
 };
 module.exports = AppList;
